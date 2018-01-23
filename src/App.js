8420b6bb3204f872274291a37893e7493f6bb7c7
Tim Kholod <artijoh@gmail.com>
2018-01-23 17:16:20 +0300

    Implement a tweet timeline

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Avatar from './Avatar';
 import InputFile from './InputFile';
 import Button from './Button';
 import {RoundButton} from './Button';
+import Tweet from './Tweet';
 import * as Icons from 'react-icons/lib/ti';
 
 import './App.css';
@@ -88,10 +89,15 @@ class App extends Component {
         login: 'timka',
         avatarUrl: 'https://pbs.twimg.com/profile_images/883637729782792193/ygU8MLl2_400x400.jpg',
         coverUrl: 'https://foreignpolicymag.files.wordpress.com/2017/09/gettyimages-142965003.jpg?w=1920&h=920&crop=0,0,540,0',
-        tweets: 786,
+        tweetsCount: 786,
         following: 345,
         followers: 195747,
       },
+      tweets: [
+        {authorName: "Tom", authorLogin: "@knke", authorAvatarUrl: "https://pbs.twimg.com/profile_images/883637729782792193/ygU8MLl2_400x400.jpg", text: "My design course now has 5 hrs of video, 27k words of lessons, 10 really detailed practice project briefs, dozens of source files, a Facebook community, and more on the way. But that's not what makes it worthwhile.", likes: 2, retweets: 5, responses: 4, publishDate: new Date},
+        {authorName: "Ann", authorLogin: "@rererve223", authorAvatarUrl: "https://www.thewrap.com/wp-content/uploads/2015/11/Donald-Trump.jpg", text: "If you’re on the fence, you can email me on this page. I’ll be honest about whether the course is right for you. I don’t want your money if the course isn’t useful.", likes: 0, retweets: 2, responses: 0, publishDate: new Date},
+        {authorName: "Alex", authorLogin: "@77736hhue", authorAvatarUrl: "https://www.alvinailey.org/sites/default/files/styles/slideshow_image/public/melanie-person.jpg?itok=ocw3xkx_", text: "You can save $50 on the course for a few more hours. It’s called TheorySprints. Use discount code “theorysprints50”. See more details at this link", likes: 5, retweets: 51, responses: 2, publishDate: new Date},
+      ],
       isActiveWriteTweetInput: false,
     };
   }
@@ -111,7 +117,7 @@ class App extends Component {
 
   render() {
     const trends = this.state.trends.find(trends => trends.country === this.state.selectedCountry);
-    const {account, isActiveWriteTweetInput} = this.state;
+    const {account, isActiveWriteTweetInput, tweets} = this.state;
 
     return (
       <div className="App">
@@ -123,7 +129,7 @@ class App extends Component {
               coverUrl={account.coverUrl}
               name={account.name}
               login={account.login}
-              tweetsCount={account.tweets}
+              tweetsCount={account.tweetsCount}
               followingCount={account.following}
               followersCount={account.followers}
             />
@@ -182,9 +188,21 @@ class App extends Component {
                   )}
               </div>
             </div>
-
+            <div className="timeline">
+              {tweets.map(tweet =>
+                <Tweet
+                  avatarUrl={tweet.authorAvatarUrl}
+                  name={tweet.authorName}
+                  text={tweet.text}
+                  login={tweet.authorLogin}
+                  responses={tweet.responses}
+                  likes={tweet.likes}
+                  retweets={tweet.retweets}
+                  publishDate={tweet.publishDate}
+                />
+              )}
+            </div>
           </div>
-
         </div>
       </div>
     );
---
+++ src/Tweet.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+
+import * as Icons from 'react-icons/lib/ti';
+import moment from 'moment';
+
+import Avatar from './Avatar';
+
+import './Tweet.css';
+
+class FromNow extends Component {
+  state = { date: new Date };
+
+  componentDidMount() {
+
+    this.num = setInterval(() => {
+      this.setState({ date: new Date });
+    }, 1000);
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.num);
+  }
+
+  render() {
+    const { datetime } = this.props;
+    return <span>{moment(datetime).fromNow()}</span>;
+  }
+}
+
+function Tweet ({ avatarUrl, name, login, text, responses, likes, retweets, publishDate }) {
+  return (
+    <div className="tweet">
+      <Avatar
+        avatarUrl={avatarUrl}
+        size={45}
+      />
+      <div>
+        <div className="text-block">
+          <div className="personal-info">
+            <div>
+              <a href="#" style={{ color: '#111', fontWeight: '600'}}>{name}</a>
+              <a href="#">@{login}</a>
+              <span>・</span>
+              <a href="#">
+                <FromNow datetime={publishDate} />
+              </a>
+            </div>
+            <div>
+              <a href="#">
+                <Icons.TiArrowSortedDown size={25} />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="tweet-text">
+          {text}
+        </div>
+        <div className="buttons">
+          <div className="response-button">
+            <Icons.TiBook size={25} />
+            {responses !== 0 ? responses : ''}
+          </div>
+          <div className="retweet-button">
+            <Icons.TiArrowRepeat size={25} />
+            {retweets !== 0 ? retweets : ''}
+          </div>
+          <div className="like-button">
+            <Icons.TiHeartOutline size={25} />
+            {likes !== 0 ? likes : ''}
+          </div>
+          <div className="mail-button">
+            <Icons.TiMail size={25} />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Tweet;
